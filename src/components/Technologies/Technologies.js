a6db72cfe573d20d2a88c0736d8b9fabd4f5e3a8
Jarrod Savard <Jarrodsavard2x180@gmail.com>
2022-05-30 12:05:57 -0700

    finished outline

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -3,10 +3,38 @@ import { DiFirebase, DiReact, DiZend } from 'react-icons/di';
 import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
 import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
 
-const Technologies = () =>  (
-  <div>
-    Technologies
-  </div>
+const Technologies = () => (
+    <Section id="technologies">
+        <SectionDivider />
+        <br />
+        <br />
+        <br />
+        <SectionTitle>Technologies</SectionTitle>
+        <SectionText>Information about the tech stack that I program in</SectionText>
+        <List>
+            <ListItem>
+                <DiReact size="3rem" />
+                <ListContainer>
+                    <ListTitle>Front-End</ListTitle>
+                    <ListParagraph>Experience with React.js</ListParagraph>
+                </ListContainer>
+            </ListItem>
+            <ListItem>
+                <DiFirebase size="3rem" />
+                <ListContainer>
+                    <ListTitle>Back-End</ListTitle>
+                    <ListParagraph>Experience with Node.js & databases</ListParagraph>
+                </ListContainer>
+            </ListItem>
+            <ListItem>
+                <DiZend size="3rem" />
+                <ListContainer>
+                    <ListTitle>UI/UX</ListTitle>
+                    <ListParagraph>Design tools such as Figma</ListParagraph>
+                </ListContainer>
+            </ListItem>
+        </List>
+    </Section>
 );
 
 export default Technologies;
