ae2f0b0c68c6fe4dca9bd968d32e28786e0cf9ca
Jarrod Savard <Jarrodsavard2x180@gmail.com>
2022-05-30 16:30:33 -0700

    updated site

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { AiFillGithub, AiFillInstagram, AiFillLinkedin } from 'react-icons/ai';
+import { AiFillGithub, AiFillLinkedin } from 'react-icons/ai';
 
 import { SocialIcons } from '../Header/HeaderStyles';
 import { CompanyContainer, FooterWrapper, LinkColumn, LinkItem, LinkList, LinkTitle, Slogan, SocialContainer, SocialIconsContainer } from './FooterStyles';
@@ -7,6 +7,12 @@ import { CompanyContainer, FooterWrapper, LinkColumn, LinkItem, LinkList, LinkTi
 const Footer = () => {
     return (
         <FooterWrapper>
+            <LinkList>
+                <LinkColumn>
+                    <LinkTitle>Email</LinkTitle>
+                    <LinkItem href="mailto:jarrodsavard2x180@gmail.com">jarrodsavard2x180@gmail.com</LinkItem>
+                </LinkColumn>
+            </LinkList>
             <SocialIconsContainer>
                 <CompanyContainer>
                     <Slogan>Innovating one project at a time</Slogan>
--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,7 +1,7 @@
 export const projects = [
     {
         title: 'E-Commerce',
-        description: 'This is a functional Ecommerce website I made with modern technologies built on NextJs code. Sanity was used as a CMS and Stripe was used for payment integration. EVERYTHING ON THE SITE IS JUST FOR SHOW AND NON-PURCHASABLE.',
+        description: 'This is a functional e-commerce website I made with modern technologies built on NextJs code. Sanity was used as a CMS and Stripe was used for payment integration. EVERYTHING ON THE SITE IS JUST FOR SHOW AND NON-PURCHASABLE.',
         image: '/images/2.png',
         tags: ['React', 'JavaScript', 'Stripe', 'Sanity'],
         source: 'https://ecommerce-site-wine.vercel.app/',
@@ -11,20 +11,29 @@ export const projects = [
     {
         title: 'Typing Test',
         description: 'Typing test written in Vanilla JavaScript. This test is made with 1000 of the most commonly used English words. On each reload the words are randomly chosen then dynamically inputted.',
-        image: '/images/3.jpg',
-        tags: ['JavaScript', 'Webpack', 'api'],
+        image: '/images/3.png',
+        tags: ['JavaScript', 'Webpack', 'API'],
         source: 'https://jarrodsavard.github.io/Typing-test/',
         visit: 'https://jarrodsavard.github.io/Typing-test/',
         id: 2,
     },
     {
-        title: 'Unichat',
-        description: 'This is a code repository for the corresponding video tutorial. In this video, we will create a full Realtime Chat Application',
-        image: '/images/4.jpg',
-        tags: ['React', 'ChatEngine', 'Firebase'],
-        source: 'https://google.com',
-        visit: 'https://google.com',
+        title: 'Simple Quiz',
+        description: 'This is a simple quiz written in Vanilla JavaScript. It includes 4 questions and a submit button. When clicked the submit button fires a function that brings you to the top of the page and another that tallies your score.  ',
+        image: '/images/4.png',
+        tags: ['JavaScript'],
+        source: 'https://jarrodsavard.github.io/Quiz/',
+        visit: 'https://jarrodsavard.github.io/Quiz/',
         id: 3,
+    },
+    {
+        title: 'Relax Web App',
+        description: 'This is a web app that is designed to help people with stress, anxiety, and/or breathing issues. This app features a visual que indicating when to breath in, hold your breath, and breath out.',
+        image: '/images/1.png',
+        tags: ['JavaScript'],
+        source: 'https://jarrodsavard.github.io/Relax/',
+        visit: 'https://jarrodsavard.github.io/Relax/',
+        id: 4,
     },
 ];
 
