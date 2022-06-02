28a3ee6e463b42812adafdc52c774832f74b4894
Jarrod Savard <Jarrodsavard2x180@gmail.com>
2022-06-02 15:56:12 -0700

    changed images to .webP for compression and preformance

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -2,7 +2,7 @@ export const projects = [
     {
         title: 'E-Commerce',
         description: 'This is a functional e-commerce website I made with modern technologies built on NextJs code. Sanity was used as a CMS and Stripe was used for payment integration. EVERYTHING ON THE SITE IS JUST FOR SHOW AND NON-PURCHASABLE.',
-        image: '/images/2.png',
+        image: '/images/2.webP',
         tags: ['React', 'JavaScript', 'Stripe', 'Sanity'],
         source: 'https://ecommerce-site-wine.vercel.app/',
         visit: 'https://ecommerce-site-wine.vercel.app/',
@@ -11,7 +11,7 @@ export const projects = [
     {
         title: 'Typing Test',
         description: 'Typing test written in Vanilla JavaScript. This test is made with 1000 of the most commonly used English words. On each reload the words are randomly chosen then dynamically inputted.',
-        image: '/images/3.png',
+        image: '/images/3.webP',
         tags: ['JavaScript', 'Webpack', 'API'],
         source: 'https://jarrodsavard.github.io/Typing-test/',
         visit: 'https://jarrodsavard.github.io/Typing-test/',
@@ -20,7 +20,7 @@ export const projects = [
     {
         title: 'Simple Quiz',
         description: 'This is a simple quiz written in Vanilla JavaScript. It includes 4 questions and a submit button. When clicked the submit button fires a function that brings you to the top of the page and another that tallies your score.  ',
-        image: '/images/4.png',
+        image: '/images/4.webP',
         tags: ['JavaScript'],
         source: 'https://jarrodsavard.github.io/Quiz/',
         visit: 'https://jarrodsavard.github.io/Quiz/',
@@ -29,7 +29,7 @@ export const projects = [
     {
         title: 'Relax Web App',
         description: 'This is a web app that is designed to help people with stress, anxiety, and/or breathing issues. This app features a visual que indicating when to breath in, hold your breath, and breath out.',
-        image: '/images/1.png',
+        image: '/images/1.webP',
         tags: ['JavaScript'],
         source: 'https://jarrodsavard.github.io/Relax/',
         visit: 'https://jarrodsavard.github.io/Relax/',
